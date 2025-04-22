72bdc718cc811dad5bf5558a542ffbeb7a80a514
anacslr <anais.cieslar@gmail.com>
2025-04-22 11:16:48 +0200

    feat: create footer

---
+++ client/src/components/footer/Footer.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <footer>
+      <div className="iconesReseaux">
+        <img src="../../assets/images/facebook.svg" alt="lien facebook" />
+        <img src="../../assets/images/x_twitter.svg" alt="lien x twitter" />
+        <img src="../../assets/images/instagram.svg" alt="lien instagram" />
+      </div>
+      <p>404 Team not found</p>
+    </footer>
+  );
+}
+export default Footer;
