827cf39c6c5eb0752792255f61b4c2641d23b6fc
anacslr <anais.cieslar@gmail.com>
2025-04-23 14:47:25 +0200

    feat: create navbar, menu burger for mobile and add navlink for other pages

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,19 +1,44 @@
+import { NavLink } from "react-router";
 import "./navbar.css";
+import { useState } from "react";
 
 function Navbar() {
+  const [showLinks, setShowLinks] = useState(false);
+
+  const handleShowlinks = () => {
+    setShowLinks(!showLinks);
+  };
   return (
-    <nav>
+    <nav className={`${showLinks ? "show-nav" : "hidden"}`}>
       <img
         className="logo"
         src="../../src/assets/images/Astralis-svg-Final.svg"
         alt="logo planete astralis"
       />
       <p className="astralis">Astralis</p>
-      <ul>
-        <li>Home</li>
-        <li>Système Solaire</li>
-        <li>Evénements</li>
+      <ul className="links">
+        <NavLink
+          to="/Home"
+          className={({ isActive }) => (isActive ? "active-link" : "")}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/SystemeSolaire"
+          className={({ isActive }) => (isActive ? "active-link" : "")}
+        >
+          Système Solaire
+        </NavLink>
+        <NavLink
+          to="/Evenements"
+          className={({ isActive }) => (isActive ? "active-link" : "")}
+        >
+          Evénements
+        </NavLink>
       </ul>
+      <button type="button" className="burger" onClick={handleShowlinks}>
+        <span className="burger-bar" />
+      </button>
     </nav>
   );
 }
