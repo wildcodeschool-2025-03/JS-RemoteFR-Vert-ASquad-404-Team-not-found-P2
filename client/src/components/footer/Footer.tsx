b787b7103c0dca638987fa3d6197b8128a88bc59
anacslr <anais.cieslar@gmail.com>
2025-04-24 12:40:40 +0200

    fix: change pictures link to import

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -1,12 +1,15 @@
 import "./footer.css";
+import facebook from "../../assets/images/facebook.svg";
+import instagram from "../../assets/images/instagram.svg";
+import x_twitter from "../../assets/images/x_twitter.svg";
 
 function Footer() {
   return (
     <footer>
       <div className="iconesReseaux">
-        <img src="../../src/assets/images/facebook.svg" alt="lien facebook" />
-        <img src="../../src/assets/images/x_twitter.svg" alt="lien x twitter" />
-        <img src="../../src/assets/images/instagram.svg" alt="lien instagram" />
+        <img src={facebook} alt="lien facebook" />
+        <img src={x_twitter} alt="lien x twitter" />
+        <img src={instagram} alt="lien instagram" />
       </div>
       <p>404 Team not found</p>
     </footer>
