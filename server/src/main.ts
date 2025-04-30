ad6d04b39c568439e7e0583ce4c05247367b60f7
AlexandreMorawek <alexandre.morawek@gmail.com>
2025-04-30 15:55:50 +0200

    feat: adding carousel on PlanetInfo page

--- server/src/main.ts
+++ server/src/main.ts
@@ -30,10 +30,10 @@ const infoSolarSystem = [
     number_of_satellites: 0,
     ring_system: false,
     img: {
-      description_img: "../client/src/assets/images/Soleil1_Page1bis.png",
-      carrousel_img1: "../client/src/assets/images/Soleil1_Page1bis.png",
-      carrousel_img2: "../client/src/assets/images/Soleil2_Page1bis.png",
-      carrousel_img3: "../client/src/assets/images/Soleil3_Page1bis.png",
+      description_img: "../public/images/Soleil1_Page1bis.png",
+      carrousel_img1: "../public/images/Soleil1_Page1bis.png",
+      carrousel_img2: "../public/images/Soleil2_Page1bis.png",
+      carrousel_img3: "../public/images/Soleil3_Page1bis.png",
     },
     description:
       "Le Soleil est l’étoile au centre du système solaire. Composé principalement d’hydrogène et d’hélium, il produit de l’énergie par fusion nucléaire en transformant l’hydrogène en hélium. Cette énergie est essentielle à la vie sur Terre, fournissant lumière et chaleur. Son diamètre dépasse 1,39 million de kilomètres, ce qui représente 109 fois celui de la Terre. Le Soleil représente plus de 99,8 % de la masse du système solaire. Sa surface, appelée photosphère, atteint environ 5 500 °C. Il n’a pas de surface solide et tourne sur lui-même en environ 25 jours à l’équateur. Il influence tous les corps du système solaire.",
@@ -58,10 +58,10 @@ const infoSolarSystem = [
     number_of_satellites: 0,
     ring_system: false,
     img: {
-      description_img: "../client/src/assets/images/Mercure1_Page1bis.png",
-      carrousel_img1: "../client/src/assets/images/Mercure1_Page1bis.png",
-      carrousel_img2: "../client/src/assets/images/Mercure2_Page1bis.png",
-      carrousel_img3: "../client/src/assets/images/Mercure3_Page1bis.png",
+      description_img: "../public/images/Mercure1_Page1bis.png",
+      carrousel_img1: "../public/images/Mercure1_Page1bis.png",
+      carrousel_img2: "../public/images/Mercure2_Page1bis.png",
+      carrousel_img3: "../public/images/Mercure3_Page1bis.png",
     },
     description:
       "Mercure est la planète la plus proche du Soleil. Petite et rocheuse, elle n’a presque pas d’atmosphère, ce qui entraîne de grandes variations de température entre le jour et la nuit. Sa surface est criblée de cratères, semblable à celle de la Lune. Mercure orbite très rapidement autour du Soleil, en seulement 88 jours terrestres. Sa proximité au Soleil la rend difficile à observer depuis la Terre. Sa rotation lente, combinée à son orbite rapide, donne lieu à des levers de soleil étonnants. Bien qu’elle soit la plus petite planète, elle possède un noyau métallique très dense.",
@@ -86,10 +86,10 @@ const infoSolarSystem = [
     number_of_satellites: 0,
     ring_system: false,
     img: {
-      description_img: "../client/src/assets/images/Venus1_Page1bis.png",
-      carrousel_img1: "../client/src/assets/images/Venus1_Page1bis.png",
-      carrousel_img2: "../client/src/assets/images/Venus2_Page1bis.png",
-      carrousel_img3: "../client/src/assets/images/Venus3_Page1bis.png",
+      description_img: "../public/images/Venus1_Page1bis.png",
+      carrousel_img1: "../public/images/Venus1_Page1bis.png",
+      carrousel_img2: "../public/images/Venus2_Page1bis.png",
+      carrousel_img3: "../public/images/Venus3_Page1bis.png",
     },
     description:
       "Vénus, souvent appelée “sœur jumelle” de la Terre à cause de sa taille similaire, possède pourtant un environnement extrêmement hostile. Son atmosphère épaisse est composée majoritairement de dioxyde de carbone et crée un effet de serre intense, faisant de Vénus la planète la plus chaude du système solaire. Sa surface est volcanique et recouverte de plaines et montagnes. Elle tourne très lentement et dans le sens inverse des autres planètes. Vénus ne possède ni lune ni champ magnétique important. Depuis la Terre, elle brille fortement dans le ciel et est souvent visible au lever ou au coucher du Soleil.",
@@ -114,10 +114,10 @@ const infoSolarSystem = [
     number_of_satellites: 1,
     ring_system: false,
     img: {
-      description_img: "../client/src/assets/images/Terre1_Page1bis.png",
-      carrousel_img1: "../client/src/assets/images/Terre1_Page1bis.png",
-      carrousel_img2: "../client/src/assets/images/Terre2_Page1bis.png",
-      carrousel_img3: "../client/src/assets/images/Terre3_Page1bis.png",
+      description_img: "../public/images/Terre1_Page1bis.png",
+      carrousel_img1: "../public/images/Terre1_Page1bis.png",
+      carrousel_img2: "../public/images/Terre2_Page1bis.png",
+      carrousel_img3: "../public/images/Terre3_Page1bis.png",
     },
     description:
       "La Terre est la seule planète connue à ce jour à abriter la vie. Elle possède une atmosphère riche en azote et oxygène, une abondance d’eau liquide, et un climat tempéré. Sa surface est composée de continents et d’océans, en perpétuel mouvement à cause des plaques tectoniques. Elle a un unique satellite naturel, la Lune, qui influence les marées. La Terre effectue une rotation sur elle-même en 24 heures et une révolution autour du Soleil en 365,25 jours. Sa biosphère diversifiée en fait un écosystème complexe et fascinant. C’est notre foyer, fragile et précieux dans l’immensité de l’espace.",
@@ -142,10 +142,10 @@ const infoSolarSystem = [
     number_of_satellites: 2,
     ring_system: false,
     img: {
-      description_img: "../client/src/assets/images/Mars1_Page1bis.png",
-      carrousel_img1: "../client/src/assets/images/Mars1_Page1bis.png",
-      carrousel_img2: "../client/src/assets/images/Mars2_Page1bis.png",
-      carrousel_img3: "../client/src/assets/images/Mars3_Page1bis.png",
+      description_img: "../public/images/Mars1_Page1bis.png",
+      carrousel_img1: "../public/images/Mars1_Page1bis.png",
+      carrousel_img2: "../public/images/Mars2_Page1bis.png",
+      carrousel_img3: "../public/images/Mars3_Page1bis.png",
     },
     description:
       "Mars est surnommée la “planète rouge” en raison de l’oxyde de fer qui recouvre sa surface. C’est une planète rocheuse, plus petite que la Terre, avec une atmosphère très mince composée majoritairement de dioxyde de carbone. Elle possède les plus grands volcans et canyons du système solaire, comme Olympus Mons et Valles Marineris. Les températures y sont froides, et bien qu’il n’y ait pas d’eau liquide à la surface aujourd’hui, de nombreuses preuves suggèrent qu’elle a coulé dans le passé. Mars a deux petites lunes, Phobos et Deimos, et elle est au cœur de nombreuses missions spatiales en quête de vie.",
@@ -170,10 +170,10 @@ const infoSolarSystem = [
     number_of_satellites: 80,
     ring_system: true,
     img: {
-      description_img: "../client/src/assets/images/Jupiter1_Page1bis.png",
-      carrousel_img1: "../client/src/assets/images/Jupiter1_Page1bis.png",
-      carrousel_img2: "../client/src/assets/images/Jupiter2_Page1bis.png",
-      carrousel_img3: "../client/src/assets/images/Jupiter3_Page1bis.png",
+      description_img: "../public/images/Jupiter1_Page1bis.png",
+      carrousel_img1: "../public/images/Jupiter1_Page1bis.png",
+      carrousel_img2: "../public/images/Jupiter2_Page1bis.png",
+      carrousel_img3: "../public/images/Jupiter3_Page1bis.png",
     },
     description:
       "Jupiter est la plus grande planète du système solaire. C’est une géante gazeuse principalement composée d’hydrogène et d’hélium, avec une atmosphère marquée par des bandes colorées et des tempêtes puissantes, comme la célèbre Grande Tache Rouge. Elle possède un champ magnétique colossal et dégage plus d’énergie qu’elle n’en reçoit du Soleil. Jupiter est entourée d’un grand nombre de lunes, dont les quatre plus grandes, appelées lunes galiléennes, comprennent Io, Europe, Ganymède et Callisto. Ces satellites présentent un grand intérêt scientifique, notamment Europe, qui pourrait abriter un océan souterrain. Jupiter n’a pas de surface solide, mais son noyau est probablement rocheux.",
@@ -198,10 +198,10 @@ const infoSolarSystem = [
     number_of_satellites: 82,
     ring_system: true,
     img: {
-      description_img: "../client/src/assets/images/Saturne1_Page1bis.png",
-      carrousel_img1: "../client/src/assets/images/Saturne1_Page1bis.png",
-      carrousel_img2: "../client/src/assets/images/Saturne2_Page1bis.png",
-      carrousel_img3: "../client/src/assets/images/Saturne3_Page1bis.png",
+      description_img: "../public/images/Saturne1_Page1bis.png",
+      carrousel_img1: "../public/images/Saturne1_Page1bis.png",
+      carrousel_img2: "../public/images/Saturne2_Page1bis.png",
+      carrousel_img3: "../public/images/Saturne3_Page1bis.png",
     },
     description:
       "Saturne est une géante gazeuse célèbre pour son système d’anneaux spectaculaires composés de glace, de roche et de poussière. Elle est principalement constituée d’hydrogène et d’hélium, et a une faible densité — elle flotterait dans l’eau si une baignoire assez grande existait ! Son atmosphère présente des bandes similaires à celles de Jupiter, bien que plus pâles. Saturne a plus de 80 lunes confirmées, dont Titan, une lune massive avec une atmosphère dense et des lacs de méthane liquide. Le système saturnien fascine les scientifiques, notamment grâce aux données recueillies par la sonde Cassini, qui a exploré la planète pendant 13 ans.",
@@ -226,10 +226,10 @@ const infoSolarSystem = [
     number_of_satellites: 27,
     ring_system: true,
     img: {
-      description_img: "../client/src/assets/images/Uranus1_Page1bis.png",
-      carrousel_img1: "../client/src/assets/images/Uranus1_Page1bis.png",
-      carrousel_img2: "../client/src/assets/images/Uranus2_Page1bis.png",
-      carrousel_img3: "../client/src/assets/images/Uranus3_Page1bis.png",
+      description_img: "../public/images/Uranus1_Page1bis.png",
+      carrousel_img1: "../public/images/Uranus1_Page1bis.png",
+      carrousel_img2: "../public/images/Uranus2_Page2bis.png",
+      carrousel_img3: "../public/images/Uranus3_Page3bis.png",
     },
     description:
       "Uranus est une géante glacée au bleu pâle distinctif, dû au méthane présent dans son atmosphère. Elle se distingue par son axe de rotation très incliné : la planète “roule” presque sur son orbite, ce qui provoque des saisons extrêmes. Composée principalement d’hydrogène, d’hélium et de glaces comme l’eau, l’ammoniac et le méthane, Uranus est entourée d’anneaux ténus et de 27 lunes connues. Elle est souvent appelée “planète oubliée” en raison de son éloignement et du peu de missions l’ayant visitée. Son atmosphère peut atteindre des températures glaciales record, faisant d’elle la planète la plus froide du système solaire.",
@@ -254,10 +254,10 @@ const infoSolarSystem = [
     number_of_satellites: 14,
     ring_system: true,
     img: {
-      description_img: "../client/src/assets/images/Neptune1_Page1bis.png",
-      carrousel_img1: "../client/src/assets/images/Neptune1_Page1bis.png",
-      carrousel_img2: "../client/src/assets/images/Neptune2_Page1bis.png",
-      carrousel_img3: "../client/src/assets/images/Neptune3_Page1bis.png",
+      description_img: "../public/images/Neptune1_Page1bis.png",
+      carrousel_img1: "../public/images/Neptune1_Page1bis.png",
+      carrousel_img2: "../public/images/Neptune2_Page1bis.png",
+      carrousel_img3: "../public/images/Neptune3_Page1bis.jpg",
     },
     description:
       "Neptune est la planète la plus lointaine du Soleil. C’est une géante glacée, semblable à Uranus, avec une couleur bleue intense. Elle est connue pour ses vents parmi les plus rapides du système solaire, atteignant plus de 2 000 km/h. Son atmosphère est composée de méthane, d’hydrogène et d’hélium. Neptune possède un système complexe de tempêtes, dont la Grande Tache Sombre observée par Voyager 2. Elle a 14 lunes, dont Triton, qui orbite en sens inverse et pourrait être un objet capturé. Malgré sa distance, Neptune influence gravitationnellement l’ensemble du système solaire et demeure un objet de fascination pour l’astronomie.",
