a4c6c2364e535c6037c6e003b81152938ad545c5
Dylan-Gerard <d.marcgerard@gmail.com>
2025-05-02 18:40:14 +0200

    feat: add spinner redirection

--- client/src/components/Lost/Lost.tsx
+++ client/src/components/Lost/Lost.tsx
@@ -1,10 +1,32 @@
+import { motion } from "framer-motion";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Lost.css";
 
 function Lost() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate("/");
+    }, 300000);
+
+    return () => clearTimeout(timer);
+  }, [navigate]);
+
   return (
-    <>
+    <div className="lost-container">
       <h1>Vous êtes arrivés dans le vide intersidéral.</h1>
-    </>
+      <motion.div
+        className="spinner"
+        animate={{ rotate: 360 }}
+        transition={{
+          duration: 1.5,
+          repeat: Number.POSITIVE_INFINITY,
+          ease: "linear",
+        }}
+      />
+    </div>
   );
 }
 
